7d77a37a8fd020f5c64890e1c7b6b8ba92be714a
Wasiu Idowu <hoxtygen@live.com>
2022-12-28 17:32:13 +0100

    Add solution to removeElement algorithm that involves linkedlist.' '

---
+++ easy/remove_elements.js
@@ -0,0 +1,55 @@
+/* 
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+ 
+
+Constraints:
+
+    The number of nodes in the list is in the range [0, 104].
+    1 <= Node.val <= 50
+    0 <= val <= 50
+
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    if (head === null) return null;
+    let current = head.next;
+    let previous = head;
+    while (current) {
+      if (current.val === val) {
+        current = current.next;
+        previous.next = current;
+      } else {
+        previous = current;
+        current = current.next;
+      }
+    }
+    if (head.val === val) return head.next;
+    return head;
+};
